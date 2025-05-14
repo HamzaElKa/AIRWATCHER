e4876807fcf13c443174eaa04adbf5889ab8529f
Artifrix <yanisgallard05@gmail.com>
2025-05-14 16:43:33 +0200

    :construction: Ajout du dataset et du gestionnaire d'evenement (pas le code)

---
+++ GestionnaireSysteme.cpp
@@ -0,0 +1,12 @@
+/*
+A programmer
+Classement des capteurs similaires
+    GetMesures(idCapteur,DateDebut,DateFin) (en utilisant celle des capteurs puis en filtrant)
+    CalculerSimilarite(mesures1,mesures2)
+    TrierCapteursParSimilarite(capteurs, mesures)
+Calcul de la moyenne de l'AQI sur une zone et période donnée
+    ChercherCapteursVoisins(Long,Lat)
+Recuperer données du dossier data
+    LoadData()
+*/
+#include "GestionnaireSysteme.h"
